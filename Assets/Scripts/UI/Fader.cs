33a9b47d03ff5985a1072ee6320e3f15d27be9ba
Dariusz Halama <dariuszhalama@gmail.com>
2020-11-28 21:31:57 +0100

    xoxo

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+    [SerializeField] private CanvasGroup _canvasGroup;
+
+    public float Duration => _duration;
+
+    public IEnumerator FadeIn()
+    {
+        gameObject.SetActive(true);
+        _canvasGroup.blocksRaycasts = true;
+
+        while (_canvasGroup.alpha < 1.0f)
+        {
+            _canvasGroup.alpha += (1.0f / _duration) * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        gameObject.SetActive(true);
+        _canvasGroup.blocksRaycasts = true;
+
+        while (_canvasGroup.alpha > 0.0f)
+        {
+            _canvasGroup.alpha -= (1.0f / _duration) * Time.deltaTime;
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    public void FadeInImmediately()
+    {
+        gameObject.SetActive(true);
+        _canvasGroup.alpha = 1.0f;
+        _canvasGroup.blocksRaycasts = true;
+    }
+
+    public void FadeOutImmediately()
+    {
+        gameObject.SetActive(false);
+        _canvasGroup.alpha = 0.0f;
+        _canvasGroup.blocksRaycasts = false;
+    }
+}
---
+++ Assets/Scripts/UI/GameplayController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    [SerializeField] private Fader _fader;
+    [SerializeField] private SceneChanger _sceneChanger;
+
+    private void Start()
+    {
+        _fader.FadeInImmediately();
+        StartCoroutine(_fader.FadeOut());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StopAllCoroutines();
+            StartCoroutine(GoToMainMenu());
+        }
+    }
+
+    private IEnumerator GoToMainMenu()
+    {
+        yield return _fader.FadeIn();
+        _sceneChanger.ChangeScene("MainMenu");
+    }
+}
